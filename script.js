9c3d6a15aace03c51b22beab717e7790b0ffa3c5
Rafael Souza <souza_rafael.97@outlook.com>
2022-06-08 09:59:03 -0300

    Corrige lint

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const email = document.querySelector('.email');
 const password = document.querySelector('.password');
 const loginBtn = document.querySelector('.login-btn');
-const check = document.querySelector('#agreement');
-const submitBtn = document.querySelector('#submit-btn');
+// const check = document.querySelector('#agreement');
+// const submitBtn = document.querySelector('#submit-btn');
 
 function loginBtnActive() {
   loginBtn.addEventListener('click', () => {
@@ -15,16 +15,14 @@ function loginBtnActive() {
 }
 loginBtnActive();
 
-
-function unlockSubmitButton() {
-  if(check.checked == true) {
-    submitBtn.disabled = false;
-  } else {
-    submitBtn.disabled = true;
-  }
-}
-unlockSubmitButton()
-
+// function unlockSubmitButton() {
+//   if(check.checked == true) {
+//     submitBtn.disabled = false;
+//   } else {
+//     submitBtn.disabled = true;
+//   }
+// }
+// unlockSubmitButton()
 
 // function submitBtnActive() {
 //   check.addEventListener('change', function(event) {
@@ -36,4 +34,3 @@ unlockSubmitButton()
 //   });
 // }
 // submitBtnActive()
-
