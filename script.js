ddc8f63908bfad7c99845b8401ccc8ba64cca101
Rafael Souza <souza_rafael.97@outlook.com>
2022-06-08 09:54:50 -0300

    Corrige requisito 3

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-const email = document.querySelector('#email');
-const senha = document.querySelector('#senha');
-const btnLogin = document.querySelector('#botao');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const loginBtn = document.querySelector('.login-btn');
+const check = document.querySelector('#agreement');
+const submitBtn = document.querySelector('#submit-btn');
 
-function login() {
-  btnLogin.addEventListener('click', () => {
-    if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+function loginBtnActive() {
+  loginBtn.addEventListener('click', () => {
+    if (email.value === 'tryber@teste.com' && password.value === '123456') {
       alert('Olá, Tryber!');
     } else {
       alert('Email ou senha inválidos.');
     }
   });
 }
+loginBtnActive();
 
-login();
+
+function unlockSubmitButton() {
+  if(check.checked == true) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+}
+unlockSubmitButton()
+
+
+// function submitBtnActive() {
+//   check.addEventListener('change', function(event) {
+//     if (event.target.checked) {
+//       submitBtn.enable();
+//     } else {
+//       submitBtn.disable();
+//     }
+//   });
+// }
+// submitBtnActive()
+
