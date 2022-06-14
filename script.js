ef47c92c3a1e3fcc57b6e5072d4a7433d15136e5
Rafael Souza <souza_rafael.97@outlook.com>
2022-06-14 20:44:39 -0300

    Adiciona Requisito 20

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const agreement = document.querySelector('#agreement');
 const submitBtn = document.querySelector('#submit-btn');
 const counter = document.getElementById('counter');
 const text = document.getElementById('textarea');
+const form = document.getElementById('#form-data');
 
 function loginBtnActive() {
   loginBtn.addEventListener('click', () => {
@@ -17,6 +18,12 @@ function loginBtnActive() {
 }
 loginBtnActive();
 
+function textareaCount() {
+  const total = text.value.length;
+  counter.innerHTML = 500 - total;
+}
+text.addEventListener('input', textareaCount);
+
 window.onload = agreement.addEventListener('click', () => {
   if (agreement.value === 'checked') {
     submitBtn.disabled = true;
@@ -25,8 +32,22 @@ window.onload = agreement.addEventListener('click', () => {
   }
 });
 
-function textareaCount() {
-  const total = text.value.length;
-  counter.innerHTML = 500 - total;
-}
-text.addEventListener('input', textareaCount);
+// const secondPage = (event) => {
+//   event.preventDefault();
+
+//   const family = $('input[name=family]:checked');
+//   const contents = $$('.subject:checked');
+//   const note = $('input[name=rate]:checked');
+//   const result = document.getElementById('#form-data');
+
+//   formUser.style.display = 'none';
+//   result.classList.toggle('display');
+//   result.innertText = `
+//   Nome: ${nome.value} ${lastname.value}
+//   Email: ${emailForm.value}
+//   Casa: ${house.value}
+//   Família: ${family.value} 
+//   Avaliação: ${note.value}
+//   Observações: ${observation.value};
+//   `;
+// };
