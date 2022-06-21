bd3bc2dc9b37e252b11c1bdfc94821e78357b256
Rafael Souza <souza_rafael.97@outlook.com>
2022-06-20 22:59:53 -0300

    Requisito 21

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const agreement = document.querySelector('#agreement');
 const submitBtn = document.querySelector('#submit-btn');
 const counter = document.getElementById('counter');
 const text = document.getElementById('textarea');
-// const form = document.getElementById('#form-data');
+const resultForm = document.querySelector('.resultForm')
 
 function loginBtnActive() {
   loginBtn.addEventListener('click', () => {
@@ -32,22 +32,63 @@ window.onload = agreement.addEventListener('click', () => {
   }
 });
 
-// const secondPage = (event) => {
-//   event.preventDefault();
+function formName() {
+  const nameForm = document.getElementById('nameForm');
+  const name = document.querySelector('#input-name');
+  const lastName = document.getElementById('input-lastname');
+  nameForm.innerText += ` ${name.value} ${lastName.value}`;
+}
+function formEmail() {
+  const emailForm = document.getElementById('emailForm');
+  const email = document.querySelector('#input-email');
+  emailForm.innerText += ` ${email.value}`;
+}
+function formHouse() {
+  const house = document.getElementById('house');
+  const houseForm = document.querySelector('#casaForm');
+  houseForm.innerText += ` ${house.value}`;
+}
+function formFamily() {
+  const familiaForm = document.getElementById('familiaForm');
+  const family = document.querySelector('input[name=family]:checked');
+  familiaForm.innerHTML += family.value;
+}
+function formMaterias() {
+  const materiaForm = document.getElementById('materiasForm');
+  const materias = document.querySelectorAll('.subject:checked');
+  const materiasValue = [];
+  for (let i = 0; i < materias.length; i += 1) {
+    materiasValue.push(materias[i].value);
+  }
+  materiaForm.innerText += ` ${materiasValue.join(', ')}`;
+}
 
-//   const family = $('input[name=family]:checked');
-//   const contents = $$('.subject:checked');
-//   const note = $('input[name=rate]:checked');
-//   const result = document.getElementById('#form-data');
+function formRate() {
+  const rateForm = document.getElementById('rateForm');
+  const labelRate = document.querySelectorAll('[name=rate]:checked');
+  const rateValue = [];
+  for (let i = 0; i < labelRate.length; i += 1) {
+    rateValue.push(labelRate[i].value);
+  }
+  rateForm.innerText += ` ${rateValue}`;
+}
 
-//   formUser.style.display = 'none';
-//   result.classList.toggle('display');
-//   result.innertText = `
-//   Nome: ${nome.value} ${lastname.value}
-//   Email: ${emailForm.value}
-//   Casa: ${house.value}
-//   Família: ${family.value}
-//   Avaliação: ${note.value}
-//   Observações: ${observation.value};
-//   `;
-// };
+function formObs() {
+  const obsForm = document.getElementById('obsForm');
+  const textarea = document.getElementById('textarea');
+  obsForm.innerText += ` ${textarea.value}`;
+}
+
+submitBtn.addEventListener('click',(e) => {
+  e.preventDefault();
+  const mainForm = document.querySelector('#evaluation-form');
+  mainForm.style.display = 'none';
+  resultForm.style.display = 'block';
+  formName();
+  formEmail();
+  formHouse();
+  formFamily();
+  formMaterias();
+  formRate();
+  formObs()
+})
